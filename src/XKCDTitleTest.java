4b4a9e55352bc925871056bfbf1fcf295ce56856
Karyn Drombosky <karyn.drombosky@gmail.com>
2015-10-20 12:31:04 -0400

    First user story code complete

---
+++ src/XKCDTitleTest.java
@@ -0,0 +1,75 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+/*As an xkcd user, I would like to see a comic, the web page title, 
+ * and other identifying information on the main page. 
+ */
+
+
+public class XKCDTitleTest {
+
+	static WebDriver driver = new FirefoxDriver();
+	
+	@Before
+	public void setUp() throws Exception{
+		driver.get("http://www.xkcd.com");
+	}
+	
+	//@After
+	//public void tearDown() throws Exception{
+//		driver.close();
+	//}
+	
+	// Given that I am on the main page, when I view the title,
+	// I see that the word “xkcd” appears.
+	@Test
+	public void testTitle() {
+		String title = driver.getTitle();
+		assertTrue(title.contains("xkcd"));
+	}
+	
+	// Given that I am on the main page, when I view the sub title,
+	// I see the words “A webcomic of romance, sarcasm,
+	// math, and language” appear.
+	@Test
+	public void testSubtitle() {
+		WebElement e = driver.findElement(By.id("slogan"));
+		String subtitle = e.getText(); 
+		assertTrue(subtitle.contains("A webcomic of romance,\nsarcasm, math, and language."));
+	}
+	
+	// Given that I am on the main page, when I view the page, I see a comic.
+	@Test
+	public void testMainComic() {
+		try{
+			driver.findElement(By.id("comic"));
+		} catch (NoSuchElementException el_err){
+			fail();
+		}
+	}
+	
+	// Given that I am on the main page, when I view the page,
+	//I see a permanent link to the presented comic.
+	@Test
+	public void testMainComicLink() {
+		String text = "Permanent link to this comic:"; 
+		WebElement e = driver.findElement(By.id("middleContainer"));
+		assertTrue(e.getText().contains(text));
+		
+	}
+	
+	// Given that I am on the main page, when I view the page,
+	//I see a warning about potential strong language.
+	@Test
+	public void testMainLanguage() {
+		String text = "this comic occasionally contains strong language"; 
+		WebElement e = driver.findElement(By.id("bottom"));
+		assertTrue(e.getText().contains(text));
+	}
+}
